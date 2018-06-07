20bc97b90d247e507dbf78890824360ccc011b60
inec <luyege+github@gmail.com>
2018-06-06 23:15:25 -0500

    'tes'

--- hello.py
+++ hello.py
@@ -2,7 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
- return '<h1>Hello World!</h1>'
+    user_agent=request.headers.get('User-Agent')
+ return '<h1>Hello World!</h1><p>%s</p>' % user-agent
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
