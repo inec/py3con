c24503a3196b6873fb1e831d9ecf2c915de70d2f
inec <luyege@gmail.com>
2018-06-11 16:05:18 -0500

    test

---
+++ mapraw.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import urllib.request, json, time
+def GoogGeoAPI(address,api="",delay=5):
+  base = r"https://maps.googleapis.com/maps/api/geocode/json?"
+  addP = "address=" + address.replace(" ","+")
+  GeoUrl = base + addP + "&key=" + api
+  response = urllib.request.urlopen(GeoUrl)
+  jsonRaw = response.read()
+  print(jsonRaw)
+  jsonData=jsonRaw
+  #jsonData = xxjson.loads(jsonRaw)
+  if jsonData['status'] == 'OK':
+    resu = jsonData['results'][0]
+    finList = [resu['formatted_address'],resu['geometry']['location']['lat'],resu['geometry']['location']['lng']]
+  else:
+    finList = [None,None,None]
+  time.sleep(delay) #in seconds
+  return finList
+test = r"1600 Amphitheatre Parkway, Mountain View, CA"
+geoR = GoogGeoAPI(address=test)
+print(geoR)
