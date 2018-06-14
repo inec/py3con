0ef6789056a3e34ed54a9113bd91e57c99048e18
inec <luyege+github@gmail.com>
2018-06-14 10:28:06 -0500

    t

---
+++ react-1.js
@@ -0,0 +1,21 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Initial State'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    // change code below this line
+    // change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};//https://learn.freecodecamp.org/front-end-libraries/react/set-state-with-this-setstate
